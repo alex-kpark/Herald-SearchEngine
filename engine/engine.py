6c8596454c3284a2f82d01edc6c0d8e65d162e76
hitaejoon <hitaejoon@gmail.com>
2017-11-05 14:22:33 +0900

    adjust engine

--- engine/engine.py
+++ engine/engine.py
@@ -1,210 +1,79 @@
 import sys
-import gensim
-import nltk
 import os
 import numpy as np
 import operator
-from nltk.corpus import stopwords
-import json
 import string
-import json_parser as jp
+from gensim import models, corpora, similarities
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import pandas as pd
+import gzip
+import re
+import time
 
-def read_docs(whole_news):
+def get_argument():
+    input_argument=[]
+    for i in range(len(sys.argv)-1):
+        input_argument.append(sys.argv[i+1].lower())
+    return input_argument
 
-    docs2 = []
-    doc_names2 = []
+def search(input_query):
+    start = time.clock()
+
+    #다큐먼트의 크기가 매우 크므로, 원본은 압축상태로 저장해놓고 파일을 오픈하면서 압축을 푼다.
+    path = "./data/NC_100mb.txt.gz"
+    with gzip.open(path, 'rb') as f:
+        content = f.read()
+
+    #저장되어있는 모델 로드
+    model = models.Word2Vec.load('model/word2vec_100mb.model')
+    end1=time.clock()
+    print("Load Word2Vec Model Time: %s"%(end1-start))
+
+    #모델 내에 존재하는 Voca.인지 확인
+    exist_in_model_query = [q for q in input_query if q in model.wv.vocab.keys()]
+    print('Exist voca : ', exist_in_model_query)
+
+    #유사도높은 단어 30개를 뽑아서, Stopwords가 아닌 것을 걸러내고 상위 2개를 추출
     stopwrds = stopwords.words('english')
-    for news in whole_news:
-        article = nltk.word_tokenize(news['article'])
-        article = [token for token in article if token not in stopwrds]
-        docs2.append(article)
-        doc_names2.append(news['url'])
+    stopwrds.extend(['us','also'])
+    similar_word = model.most_similar(positive=exist_in_model_query,topn=20)
+    similar_word = [ sw for sw in similar_word if sw[0] is not None and sw[0] not in stopwrds]
+    new_query = exist_in_model_query[:]
+    new_query.extend(sw[0] for sw in similar_word[:2])
+    print('New query : ', new_query)
+    end2=time.clock()
+    print("Get most_similar Time: %s"%(end2-end1))
 
-    return doc_names2, docs2
+    #Series
+    #key: url / value: title, article
+    whole_news = pd.read_json(content.decode('utf-8'), typ='series', orient='records')
+    original_data = pd.Series((re.sub('[!"#%\'()*+,-./:;<=>?@\[\]\\xa0$^_`{|}~1234567890’”“′‘\\\]',' ', news['title']+" "+news['article']) for news in whole_news)
+                                , index = [news['url'] for news in whole_news])
+    end3=time.clock()
+    print("Read news(JSON) & Pandas Series Time: %s"%(end3-end2))
 
-def index_doc(docs,doc_names):
-    index = {}
-    inverse_index = {}
-    for doc,doc_name in zip(docs,doc_names):
-        word_count={}
-        for word in doc:
-            if word in word_count.keys():
-                word_count[word]+=1
-            else:
-                word_count[word]=1
-        index[doc_name] = word_count
-        #index[doc_name]=nltk.Text(doc).vocab()
-    for doc in index.keys():
-        doc_index = index[doc]
-        for word in doc_index.keys():
-            if word in inverse_index.keys():
-                inverse_index[word].append(doc)
-            else:
-                inverse_index[word] = [doc]
-    return index, inverse_index
+    #Lower & Tokenizing
+    processed_data = original_data.apply(lambda x: str(x).lower())
+    processed_data = processed_data.apply(lambda x: word_tokenize(x))
+    dictionary = corpora.Dictionary(processed_data.values)
+    end4=time.clock()
+    print("Lower & Tokenizing & Dictionary Time: %s"%(end4-end3))
+    print(dictionary)
+    corpus = [dictionary.doc2bow(doc) for doc in processed_data.values]
 
-def build_dictionary(index):
-    dictionary = {}
-    for word in index.keys():
-        dictionary[word]=len(dictionary)
-    return dictionary
+    tfidf = models.TfidfModel(corpus)
+    corpus_tfidf = tfidf[corpus]
 
-def compute_tfidf(index,word_dictionary,doc_dictionary):
-    vocab_size = len(word_dictionary)
-    doc_size = len(doc_dictionary)
-    tf = np.zeros((doc_size,vocab_size))
-    for doc in index:
-        index_per_doc = index[doc]
-        vector = np.zeros(vocab_size)
-        for word in index_per_doc:
-            vector[word_dictionary[word]] = index_per_doc[word]
-        vector = np.log(vector+1)
-        tf[doc_dictionary[doc]] = vector
-    idf_numerator = doc_size
-    idf_denominator = np.sum(np.sign(tf),0)
-    idf = np.log(idf_numerator/idf_denominator)
-    tfidf = tf*idf
-    return tfidf
+    #vec_bow = dictionary.doc2bow(input_query)
+    #vec_tfidf = tfidf[vec_bow]
 
-def cosine_similarity(x,y):
-    normalizing_factor_x = np.sqrt(np.sum(np.square(x)))
-    normalizing_factor_y = np.sqrt(np.sum(np.square(y)))
-    return np.matmul(x,np.transpose(y))/(normalizing_factor_x*normalizing_factor_y)
-
-def query_matching(inverse_dictionary,query):
-
-    set_list = [set(inverse_dictionary[word]) for word in query]
-
-    return set.intersection(*set_list)
-
-def query_dot(query,word_dictionary,doc_dictionary):
-    vocab_size = len(word_dictionary)
-    doc_size=len(doc_dictionary)
-    vector = np.zeros(vocab_size)
-    for word in query:
-        if(word in word_dictionary):
-            vector[word_dictionary[word]] = 1
-    vector = np.log(vector+1)
-    idf_numerator = doc_size
-    idf_denominator = np.sum(np.sign(vector),0)
-
-    if(not(idf_denominator ==0)):
-        idf = np.log(idf_numerator/idf_denominator)
-        tfidf = vector*idf
-        return tfidf
-    else: ## if there is no match result
-        return []
-
-
-def dictionary_vector(vector,doc_dictionary):
-    dictionary={}
-    result=[]
-    doc_size=len(doc_dictionary)
-
-    for a in range(doc_size):
-        for key in doc_dictionary.keys():
-            if doc_dictionary[key]==a:
-                dictionary[key] = vector[a]
-    vector = sorted(dictionary.items(), key=operator.itemgetter(1), reverse = True)
-    for a in range(5):
-        result.append(vector[a])
-    return result
-
-def score(tfidf,word_dictionary,doc_dictionary,matched_document,query):
-    result={}
-    result2=[]
-    for a in matched_document:
-        score=0
-        for b in query:
-            wordnum = word_dictionary[b]
-            score += tfidf[doc_dictionary[a]][wordnum]
-        result[a]=score
-
-    result = sorted(result.items(), key=operator.itemgetter(1),reverse=True)
-    for a in range(5):
-        result2.append(result[a])
-
-    return result2
-
-
-def search_engine(query):
-
-    path = "./data/newsCrawl_100mb.json"
-    whole_news = jp.read_news(path)
-
-    doc_names, docs = read_docs(whole_news)
-
-    index, inverted_index = index_doc(docs,doc_names)
-    #matched_document = query_matching(inverted_index,query)
-    word_dictionary = build_dictionary(inverted_index)
-    doc_dictionary = build_dictionary(index)
-
-    tfidf = compute_tfidf(index,word_dictionary,doc_dictionary)
-
-    #score2= score(tfidf,word_dictionary,doc_dictionary,matched_document,query)
-
-    dot = query_dot(query,word_dictionary,doc_dictionary)
-
-    if(len(dot)==0):
-        return [] ## no match result
-    cos = cosine_similarity(tfidf,dot)
-    vector = dictionary_vector(cos,doc_dictionary)
-
-    result2 =[]
-
-    for a in range(len(vector)):
-        result2.append(vector[a][0])
-
-    return result2
-
+    #print(vec_bow)
+    #print(tfidf[vec_bow])
+    #print(dictionary.items())
 
 if __name__ == "__main__":
-
-    model = gensim.models.Word2Vec.load('model/word2vec.model')
-
-    input_query = []
-    for i in range(len(sys.argv)-1):
-        input_query.append(sys.argv[i+1])
-
+    input_query = get_argument()
     print('Input query : ', input_query)
 
-    result = []
-    stopwrds = stopwords.words('english')
-
-    if(not(input_query=="")):
-        tokens=[]
-        for c in range(len(input_query)):
-            tokens.append(input_query[c])
-        query=[]
-
-        for c in range(len(tokens)):
-            if(tokens[c] in model.wv.vocab):
-                query.append(tokens[c].lower())
-
-        if(not(len(query)==0)):
-            similar_word = model.most_similar(positive=query,topn=10)
-
-            if(len(similar_word)==0):
-                query = search_engine(query)
-            else:
-                print('hidden layer')
-                print(similar_word)
-
-                for c in range(len(similar_word)):
-                    if(similar_word[c][0] not in stopwrds):
-                        query.append(similar_word[c][0])
-
-                print('New query')
-                print(query)
-
-                for c in range(len(query)):
-                    query[c]= query[c].lower() ## Lower case
-
-                documents = search_engine(query)
-        else:
-            documents = search_engine(tokens)
-
-        if(len(query)==0):
-            print('No result...')
-        else:
-            print('Most similar documents : ', documents)
+    search(input_query)
--- engine/make_model.py
+++ engine/make_model.py
@@ -5,17 +5,23 @@ warnings.filterwarnings(action='ignore', category=UserWarning, module='gensim')
 import gensim
 import multiprocessing
 import os
-import json
 import string
-from nltk.corpus import stopwords
+import gzip
+import pandas as pd
+import re
+from gensim import corpora
+from nltk.tokenize import word_tokenize
 
+def make_model(sentences):
+    whole_news = pd.read_json(content, typ='series', orient='records')
 
-def make_model():
+    sentences = pd.Series(re.sub('[!"#%\'()*+,-./:;<=>?@\[\]\\xa0$^_`{|}~1234567890’”“′‘\\\]',' ', news['title']+" "+news['article']) for news in whole_news)
 
-    sentences_vocab = gensim.models.word2vec.Text8Corpus('./traindata')
-    #print(sentences_vocab)
-    #sentences_vocab = SentenceReader('./docs')
-    #sentences_train = SentenceReader('./docs')
+    sentences = sentences.apply(lambda x: str(x).lower())
+    sentences = sentences.apply(lambda x: word_tokenize(x))
+
+    dictionary = corpora.Dictionary(sentences)
+    corpus = [dictionary.doc2bow(doc) for doc in sentences]
 
     config = {
         'min_count': 5,  # 등장 횟수가 5 이하인 단어는 무시
@@ -25,15 +31,26 @@ def make_model():
         'iter': 10,  # 보통 딥러닝에서 말하는 epoch과 비슷한, 반복 횟수
         'workers': multiprocessing.cpu_count(),
     }
-    model = gensim.models.word2vec.Word2Vec(sentences_vocab)
-    #model.build_vocab(sentences_vocab)
-    #model.train(sentences_train)
-
-    model.save('model/word2vec.model')
+    model = gensim.models.word2vec.Word2Vec(sentences.values, size=200, window=5, iter=10, min_count=5, workers=multiprocessing.cpu_count())
+    model.save('model/word2vec_100mb.model')
     print('Saved')
 
     return model
 
 if __name__ == '__main__':
+    
+    path = "./data/NC_100mb.txt.gz"
+    with gzip.open(path, 'rb') as f:
+        content = f.read()
+    make_model(content.decode('utf-8'))
 
-    model = make_model()
+
+
+    '''
+    file = open('./data/NC_100mb.json', 'rt', encoding='utf-8')
+    content = file.read()
+
+    f = gzip.open('./data/NC_100mb.txt.gz', 'wb')
+    f.write(content.encode('utf-8'))
+    f.close()
+    '''
