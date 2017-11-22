782c0e7203d1f21de237012463a231e1636452c8
hitaejoon <hitaejoon@gmail.com>
2017-11-23 02:36:55 +0900

    Make outline of giving Weight

--- engine/classifier.py
+++ engine/classifier.py
@@ -4,8 +4,6 @@ import pandas as pd
 import collections
 
 import keras
-from keras.models import Sequential
-from keras.layers import Dense, LSTM
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing import sequence
 from sklearn.preprocessing import LabelEncoder
@@ -15,15 +13,16 @@ import pickle
 
 import time
 import gzip
-import re
 
-def predict_category(query):
-
-    # Load data
+def predict_category(content, query):
+    '''
     path = "./data/NC_70mb.json.gz"
     with gzip.open(path, 'rb') as f:
         content = f.read()
-    df = pd.read_json(content.decode('utf-8'), typ='series', orient='records')
+    content = content.decode('utf-8')
+    '''
+
+    df = pd.read_json(content, typ='series', orient='records')
 
     labels = []
     for news in df:
@@ -34,36 +33,19 @@ def predict_category(query):
     trafomed_labels = le.fit_transform(labels)
 
     # Load LSTM Classification Model
-    json_file = open('model/LSTM_classifier.json', 'r')
+    json_file = open('model/LSTM_classifier_binary.json', 'r')
     loaded_model_json = json_file.read()
     json_file.close()
     model = model_from_json(loaded_model_json)
-	model.compile(loss='binary_crossentropy',
+    model.compile(loss='binary_crossentropy',
 				  optimizer='adam',
 				  metrics=['accuracy'])
-    model.load_weights("model/LSTM_classifier_weights.h5")
-    with open('./model/LSTM_tokenizer.pickle', 'rb') as handle:
+    model.load_weights("model/LSTM_classifier_binary_weights.h5")
+    with open('./model/LSTM_tokenizer_binary.pickle', 'rb') as handle:
         tokenizer = pickle.load(handle)
     t = tokenizer.texts_to_sequences(query)
     t = sequence.pad_sequences(t, maxlen=50)
-    
     prediction = model.predict_classes(np.array(t))
-    label = le.inverse_transform(prediction)
-    print("Category Label : ",label)
+    predicted_category = le.inverse_transform(prediction)
 
-    return label
-
-
-def get_argument():
-    input_argument=[]
-    for i in range(len(sys.argv)-1):
-        text = re.sub('[!"#%\'()*+,./:;<=>?@\[\]\\xa0$^_`{|}~’”“′‘\\\]',' ', sys.argv[i+1].lower())
-        input_argument.append(text)
-    return input_argument
-
-if __name__ == "__main__":
-
-    input_query = get_argument()
-    category = predict_category(input_query)
-
-    #print(category)
+    return predicted_category
--- engine/engine_doc2vec.py
+++ engine/engine_doc2vec.py
@@ -4,40 +4,33 @@ import re
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
-import gzip
+
 import time
 import gensim
 
 from operator import itemgetter, attrgetter
 
-def get_argument():
-    input_argument = []
-    for i in range(len(sys.argv) - 1):
-        input_argument.append(sys.argv[i + 1].lower())
-    return input_argument
+#def get_argument():
+#    input_argument = []
+#    for i in range(len(sys.argv) - 1):
+#        input_argument.append(sys.argv[i + 1].lower())
+#    return input_argument
 
-def read_JSON(path):
-    with gzip.open(path, 'rb') as f:
-        content = f.read()
-    return content.decode('utf-8')
+#def read_JSON(path):
+#    with gzip.open(path, 'rb') as f:
+#        content = f.read()
+#    return content.decode('utf-8')
 
 class NewsAndWeights(object):
     def __init__(self,news,weight=0):
         self.news = news
-        self.weight = weight
-
+        self._weight = weight
     def addWeight(self, weight):
-        self.weight += weight
-
+        self._weight += weight
     def getNews(self):
         return self.news
-
     def getWeight(self):
-        return self.weight
-
-    def getKey(self):
-        return self.weight
-
+        return self._weight
     def __repr__(self):
         return '{}: {} {}'.format(self.__class__.__name__,
                                   self.news['title'],
@@ -47,48 +40,67 @@ class NewsAndWeights(object):
             return self.getKey().__cmp__(other.getKey())
 
 
-path = "./data/NC_70mb.json.gz"
-content = read_JSON(path)
-
-#Series
-whole_news = pd.read_json(content, typ='series', orient='records')
-original_data = pd.Series([news for news in whole_news], index = [news['url'] for news in whole_news])
-
-def search(input_query):
+def search(content, query):
+    # Series
+    whole_news = pd.read_json(content, typ='series', orient='records')
+    original_data = pd.Series([news for news in whole_news], index = [news['url'] for news in whole_news])
     # Word2Vec을 이용해 관련도높은 단어 추출 (Hidden Query 생성)
     # -> 정확도측면에서 효과 미비하여 Deprecated
-    #similar_word = similar_words_as_word2vec(input_query)
-    #input_query.extend(sw[0] for sw in similar_word[:1])
+    # similar_word = similar_words_as_word2vec(input_query)
+    # input_query.extend(sw[0] for sw in similar_word[:1])
 
-    
-    #저장되어있는 모델 로드
+    # Load Doc2Vec model
     model = Doc2Vec.load('model/doc2vec_NC_70mb.model')
-    #end2=time.clock()
-    #print("Load Doc2Vec Model Time: %s"%(end2-end1))
 
-    #입력된 쿼리와 유사도높은 문서 추출
-    infer_vector = model.infer_vector(input_query)
+    infer_vector = model.infer_vector(query)
     similar_docs = model.docvecs.most_similar([infer_vector], topn = 200)
 
+    # TODO: 상수값 조정
+    WEIGHT_SIMILARITY = 1.0
+    WEIGHT_TERM_FREQUENCY = 0.05
+    WEIGHT_LATEST = 0.4
+    WEIGHT_CATEGORY = 1
+
+    news_weight_list = makeListAndAddWeightSimilarity(original_data, similar_docs, WEIGHT_SIMILARITY)
+    addWeightTermFrequency(news_weight_list, query, WEIGHT_TERM_FREQUENCY) #input_query: Tokenized 된 쿼리여야 함 ex. ['Trump', 'economy', 'polices']
+    addWeightCategory(news_weight_list, content, query, WEIGHT_CATEGORY) #input_query: string 형태 ex. "Trump economy polices"
+    addWeightLatest(news_weight_list, WEIGHT_LATEST)
+    
+    sorted_docs_weight = sorted(news_weight_list, key=attrgetter('_weight'), reverse=True)
+
+    return sorted_docs_weight
+
+def makeListAndAddWeightSimilarity(original_data, similar_docs, weight):
     news_weight_list = []
     for idx, s in enumerate(similar_docs):
         similarity = s[1] #similarity
         news = original_data.loc[s[0]] #key of dict: URL of news
-        news_weight_list.append(NewsAndWeights(news, similarity))
-    
-    
-    WEIGHT_TERM_FREQUENCY = 0.05
+        news_weight_list.append(NewsAndWeights(news, similarity * weight))
+    return news_weight_list
+
+def addWeightTermFrequency(newsAndWeights, input_query, weight):
     for terms in input_query:
-        for news_weight in news_weight_list:
-            count = news_weight.news['title'].count(terms) + news_weight.news['article'].count(terms)
-            news_weight.addWeight(count * WEIGHT_TERM_FREQUENCY)
-    
-    sorted_docs_weight = sorted(news_weight_list, key=attrgetter('weight'), reverse=True)
+        for nw in newsAndWeights:
+            count = nw.news['title'].count(terms) + nw.news['article'].count(terms)
+            nw.addWeight(count * weight)
+    return
 
+def addWeightCategory(newsAndWeights, content, input_query, weight):
+    import classifier
+    string_query = ""
+    for query in input_query:
+        text = re.sub('[!"#%\'()*+,./:;<=>?@\[\]\\xa0$^_`{|}~’”“′‘\\\]',' ', query.lower())
+        string_query += query + " "
+    predicted_category = classifier.predict_category(content, [string_query])
+    for nw in newsAndWeights:
+        if nw.news['category'] == predicted_category[0]:
+            nw.addWeight(weight)
+    return
 
-    for idx, news_weight in enumerate(sorted_docs_weight):
-        news = news_weight.getNews()
-        print("%d." % (idx+1)," %s" % news['title']," %s" % news['category'], " weight: %f" % news_weight.getWeight())
+def addWeightLatest(newsAndWeights, weight):
+
+    return
+
 
 def similar_words_as_word2vec(input_query):
     #저장되어있는 모델 로드
@@ -107,8 +119,10 @@ def similar_words_as_word2vec(input_query):
 
     return similar_word
 
+'''
 if __name__ == "__main__":
     input_query = get_argument()
     print('Input query : ', input_query)
 
     search(input_query)
+'''
