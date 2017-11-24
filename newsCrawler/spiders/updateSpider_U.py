32af4d6b6f039b2ba3f0d894a77276fa38c690e1
yoooonheeee <stream3004@korea.ac.kr>
2017-11-24 12:32:38 +0900

    Update updateSpider_U.py

--- newsCrawler/spiders/updateSpider_U.py
+++ newsCrawler/spiders/updateSpider_U.py
@@ -3,20 +3,13 @@
 # Please refer to the documentation for information on how to create and manage
 # your spiders.
 import scrapy
-from webcrawler.items import webcrawlerItem
+from newsCrawler.items import newsCrawlerItem
 
 class UpdateUrlSpider(scrapy.Spider):
     name = "updateSpider_U"
         
     def start_requests(self):
         # 순서대로 National, Business, Life&Style, Entertainment, Sports, World, Opinion
-        #(urls = 'http://www.koreaherald.com/list.php?ct=020100000000&ctv=0&np='
-        # urls = 'http://www.koreaherald.com/list.php?ct=020200000000&ctv=0&np='
-        # urls = 'http://www.koreaherald.com/list.php?ct=020300000000&ctv=0&np='
-        # urls = 'http://www.koreaherald.com/list.php?ct=020400000000&ctv=0&np='
-        # urls = 'http://www.koreaherald.com/list.php?ct=020500000000&ctv=0&np='
-        # urls ='http://www.koreaherald.com/list.php?ct=021200000000&ctv=0&np='
-        # urls ='http://www.koreaherald.com/list.php?ct=020600000000&ctv=0&np=' )
         urls =  [   'http://www.koreaherald.com/list.php?ct=020100000000&ctv=0&np=',
                     'http://www.koreaherald.com/list.php?ct=020200000000&ctv=0&np=',
                     'http://www.koreaherald.com/list.php?ct=020300000000&ctv=0&np=',
@@ -25,12 +18,12 @@ class UpdateUrlSpider(scrapy.Spider):
                     'http://www.koreaherald.com/list.php?ct=021200000000&ctv=0&np=',
                     'http://www.koreaherald.com/list.php?ct=020600000000&ctv=0&np=']
         for u in urls:
-            for i in range (1,4): # range 범위:National= 3184, Business= 2995, Life&Style= 926, Entertainment=868, Sports=638, World=1388, Opinion= 920
+            for i in range (1,4): # about 3 pages in a day
                  yield scrapy.Request(url= u + str(i) , callback=self.parse)
 
     def parse(self, response):
         for path in response.xpath('body/div/div[3]/div[2]/div/ul[1]/li'):   
-                item = webcrawlerItem()
+                item = newsCrawlerItem()
                 #tmpurl = path.xpath('//div[2]/p[1]/a/@href').extract()
                 #tmpurl = str(tmpurl)
                 #item['date'] = str(path.xpath('//div[2]/p[1]/a/@href').extract())[44:52]
